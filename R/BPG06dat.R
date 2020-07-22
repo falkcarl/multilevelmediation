58c8c11ffb533d18319c5f9c40694c2549ff39e8
falkcarl <carl.falk@mcgill.ca>
2020-07-22 15:16:35 -0400

    update to data

---
+++ R/BPG06dat.R
@@ -0,0 +1,13 @@
+#' Simulated dataset from Bauer, Preacher, and Gil (2006)
+#'
+#' Some description here
+#'
+#' @format A data frame with 800 observations (from 100 subjects) and 4 variables
+#' \describe{
+#'   \item{id}{Level 2 or subject ID}
+#'   \item{x}{Predictor}
+#'   \item{m}{Mediator}
+#'   \item{y}{Outcome}
+#' }
+#' @source \url{https://psycnet.apa.org/doi/10.1037/1082-989X.11.2.142}
+"BPG06dat"
