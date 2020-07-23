f334bed5e8ca6fb3c2e5c9641727837216f48e2e
falkcarl <carl.falk@mcgill.ca>
2020-07-22 23:39:31 -0400

    docs regen and update

--- R/BPG06dat.R
+++ R/BPG06dat.R
@@ -1,6 +1,7 @@
 #' Simulated dataset from Bauer, Preacher, and Gil (2006)
 #'
-#' Some description here
+#' In supplementary materials, Bauer, Preacher, and Gil (2006) provide a simulated dataset in SAS format.
+#' That dataset is replicated here.
 #'
 #' @format A data frame with 800 observations (from 100 subjects) and 4 variables
 #' \describe{
@@ -10,4 +11,6 @@
 #'   \item{y}{Outcome}
 #' }
 #' @source \url{https://psycnet.apa.org/doi/10.1037/1082-989X.11.2.142}
+#' @references
+#' Bauer, D. J., Preacher, K. J., & Gil, K. M. (2006). Conceptualizing and testing random indirect effects and moderated mediation in multilevel models: New procedures and recommendations. Psychological Methods, 11(2), 142–163. \url{https://doi.org/10.1037/1082-989X.11.2.142}
 "BPG06dat"
