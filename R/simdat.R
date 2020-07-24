b95a70484065640fd9d102aec2bacc09e2881727
falkcarl <carl.falk@mcgill.ca>
2020-07-24 17:36:34 -0400

    add simulated data w/ moderator, regen docs

---
+++ R/simdat.R
@@ -0,0 +1,15 @@
+#' A simulated dataset with moderator
+#'
+#' This simulated dataset contains a hypothetical moderating variable that is dichotomously coded.
+#' The true model dictated that the moderator moderated both a and b paths of the mediational model,
+#' though the strength of moderation may be small.
+#'
+#' @format A data frame with 800 observations (from 50 subjects) and 5 variables
+#' \describe{
+#'   \item{L2id}{Level 2 or subject ID}
+#'   \item{X}{Predictor}
+#'   \item{M}{Mediator}
+#'   \item{Y}{Outcome}
+#'   \item{mod}{Moderator}
+#' }
+"simdat"
