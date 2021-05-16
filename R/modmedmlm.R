692ae523f5e436df57c24c3c7b5c766f5d10a4b6
falkcarl <carl.falk@mcgill.ca>
2021-05-15 23:01:04 -0400

    add stack.bpg

--- R/modmedmlm.R
+++ R/modmedmlm.R
@@ -355,66 +355,20 @@ modmed.mlm<-function(data, L2ID, X, Y, M,
 
   # Some input checking per Todd's code:
   # TV: These input checks will happen at every loop of the bootstrap.
-  # TV: Is there a more efficient way? Or is the slowdown negligible? CFF: probably not that slow due to this
+  # TV: Is there a more efficient way? Or is the slowdown negligible?
+  # CFF: probably not that slow due to this; restructuring the data might slow things down, but
+  #  avoiding that might be tricky to do while bootstrapping
 
-  # Stop if X, Y, or M variables in data are not numeric
-  # factors not currently able to be used to set up BPG syntax for lme model (is there a possible workaround?)
-  if (!is.numeric(data[[X]])) {stop("X is of type ", class(data[[X]]), ". Currently, only numeric X is supported.")}
-  if (!is.numeric(data[[Y]])) {stop("Y is of type ", class(data[[Y]]), ". Y must be numeric to fit model.")}
-  if (!is.numeric(data[[M]])) {stop("M is of type ", class(data[[M]]), ". M must be numeric to fit model.")}
-
-
-  # Use data frame with only relevant variables
-  tmp <- data.frame(
-    X = data[[X]],
-    Y = data[[Y]],
-    M = data[[M]],
-    L2id = data[[L2ID]], # Save copy of the grouping (Level 2) variable
-    Md = data[[M]] # save copy of mediator
-  )
-
-  # Check and save moderator if necessary
-  if (!is.null(moderator)) {
-    # Check name and data type of moderator
-    if (!(moderator %in% colnames(data))) {stop("Moderator not found in dataset. Please check specified name.")}
-    if (!is.numeric(data[[moderator]])) {stop("Moderator is of type ", class(data[[moderator]]), ". Currently, only numeric moderators are supported.")}
-
-    # Save copy of the moderator
-    tmp$W <- data[[moderator]]
-  } else if (any(mod.a, mod.b, mod.cprime)) {
+  if (is.null(moderator) && any(mod.a, mod.b, mod.cprime)) {
     # Give error if paths indicated as moderated, but no moderator name given
     stop("No moderator was specified for the moderated path(s).")
   }
 
-  # Add covariates for M and Y outcome paths
-  # (covars.m and covars.y should be a vector of column names in the dataset)
-  if (!is.null(covars.m)) {
-    # Check if covariate names are the same as existing vars
-    if (any(covars.m %in% c(X, Y, M, moderator))) {
-      stop("Covariate(s) have the same name as X, Y, M, or moderator variables.")
-    }
-    # Save copy of covariate data
-    tmp[covars.m] = data[covars.m] #will add same variable twice if same name in list (eg c("cov1", "cov2", "cov2")) (although renames by adding a ".1" to it, so may not be a problem when lme is run?)
-  }
-  if (!is.null(covars.y)) {
-    # Check if covariate names are the same as existing vars
-    if (any(covars.y %in% c(X, Y, M, moderator))) {
-      stop("Covariate(s) have the same name as X, Y, M, or moderator variables.")
-    }
-    # Save copy of covariate data (still works if same covar is named for both m and y outcomes)
-    tmp[covars.y] = data[covars.y]
-  }
-
-  # restructure data such that both m and y are in the Z column
-  tmp <- pivot_longer(tmp, cols = c(Y, M), names_to = "Outcome",
-                      values_to = "Z")
-
-  # create variables similar to Bauer et al syntax
-  tmp$Sy <- ifelse(tmp$Outcome == "Y", 1, 0)
-  tmp$Sm <- ifelse(tmp$Outcome == "M", 1, 0)
-  tmp$SmX <- tmp$Sm * tmp$X
-  tmp$SyX <- tmp$Sy * tmp$X
-  tmp$SyM <- tmp$Sy * tmp$Md
+  tmp <- stack.bpg(data, L2ID, X, Y, M,
+                   moderator=moderator,
+                   covars.m = covars.m,
+                   covars.y = covars.y
+                   )
 
   # Create the formula for the fixed effects
   fixed.formula <- "Z ~ 0 + Sm + Sy + SmX + SyX + SyM" #use the default formula from BPG 2006
---
+++ R/utils.R
@@ -0,0 +1,111 @@
+#' Stacks data in the style of Bauer-Preacher-Gil for multilevel mediation
+#'
+#' @param data Data frame in long format.
+#' @param L2ID (String) Name of column that contains grouping variable in \code{data} (e.g., \code{"SubjectID"}).
+#' @param X (String) Name of column that contains the X independent variable in \code{data}.
+#' @param Y (String) Name of column that contains the Y dependent variable in \code{data}.
+#' @param M (String) Name of column that contains the M mediating variable in \code{data}.
+#' @param moderator Optional Character that contains name of column that contains the moderator variable in \code{data}
+#' @param covars.m (Character vector) Optional covariates to include in the model for M.
+#' @param covars.y (Character vector) Optional covariates to include in the model for Y.
+#' @details This is a convenience function used primarily internally by the package to restructure data in the style of
+#'   Bauer, Preacher, and Gil (2006). The point is to allow both Y and M to be outcomes in a single column (`Z`),
+#'   so that both mediator and outcome models can be fit at the same time. This is necessary to estimate the covariance
+#'   between "a" and "b" paths at the same time when both have random effects. Two selector variables, `Sy` and `Sm` toggle
+#'   whether each row corresponds to the outcome or the mediator, respectively.
+#' @examples
+#' \donttest{
+#'
+#' # restructure BPG data
+#' data(BPG06dat)
+#'
+#' dat <- stack.bpg(BPG06dat,
+#'   "id", "x", "m", "y"
+#' )
+#'
+#' head(dat)
+#'
+#' # restructure simulated data w/ moderator
+#' data(simdat)
+#' dat2 <- stack.bpg(simdat,
+#'   "L2id", "X", "M", "Y",
+#'   moderator="mod"
+#' )
+#'
+#' head(dat2)
+#'
+#' }
+#' @importFrom tidyr pivot_longer
+#' @export stack.bpg
+#' @usage stack.bpg(data, L2ID, X, Y, M,
+#'   moderator = NULL,
+#'   covars.m = NULL,
+#'   covars.y = NULL)
+stack.bpg <-function(data, L2ID, X, Y, M,
+                     moderator = NULL,
+                     covars.m = NULL,
+                     covars.y = NULL){
+
+  # Stop if X, Y, or M variables in data are not numeric
+  # factors not currently able to be used to set up BPG syntax for lme model (is there a possible workaround?)
+  if (!is.numeric(data[[X]])) {stop("X is of type ", class(data[[X]]), ". Currently, only numeric X is supported.")}
+  if (!is.numeric(data[[Y]])) {stop("Y is of type ", class(data[[Y]]), ". Y must be numeric to fit model.")}
+  if (!is.numeric(data[[M]])) {stop("M is of type ", class(data[[M]]), ". M must be numeric to fit model.")}
+
+
+  # Use data frame with only relevant variables
+  tmp <- data.frame(
+    X = data[[X]],
+    Y = data[[Y]],
+    M = data[[M]],
+    L2id = data[[L2ID]], # Save copy of the grouping (Level 2) variable
+    Md = data[[M]] # save copy of mediator
+  )
+
+  # Check and save moderator if necessary
+  if (!is.null(moderator)) {
+    # Check name and data type of moderator
+    if (!(moderator %in% colnames(data))) {stop("Moderator not found in dataset. Please check specified name.")}
+    if (!is.numeric(data[[moderator]])) {stop("Moderator is of type ", class(data[[moderator]]), ". Currently, only numeric moderators are supported.")}
+
+    # Save copy of the moderator
+    tmp$W <- data[[moderator]]
+  }# else if (any(mod.a, mod.b, mod.cprime)) {
+    # Give error if paths indicated as moderated, but no moderator name given
+  #  stop("No moderator was specified for the moderated path(s).")
+  #}
+
+  # Add covariates for M and Y outcome paths
+  # (covars.m and covars.y should be a vector of column names in the dataset)
+  if (!is.null(covars.m)) {
+    # Check if covariate names are the same as existing vars
+    if (any(covars.m %in% c(X, Y, M, moderator))) {
+      stop("Covariate(s) have the same name as X, Y, M, or moderator variables.")
+    }
+    # Save copy of covariate data
+    tmp[covars.m] = data[covars.m] #will add same variable twice if same name in list (eg c("cov1", "cov2", "cov2")) (although renames by adding a ".1" to it, so may not be a problem when lme is run?)
+  }
+  if (!is.null(covars.y)) {
+    # Check if covariate names are the same as existing vars
+    if (any(covars.y %in% c(X, Y, M, moderator))) {
+      stop("Covariate(s) have the same name as X, Y, M, or moderator variables.")
+    }
+    # Save copy of covariate data (still works if same covar is named for both m and y outcomes)
+    tmp[covars.y] = data[covars.y]
+  }
+
+  # restructure data such that both m and y are in the Z column
+  tmp <- pivot_longer(tmp, cols = c(Y, M), names_to = "Outcome",
+                      values_to = "Z")
+
+  # create variables similar to Bauer et al syntax
+  tmp$Sy <- ifelse(tmp$Outcome == "Y", 1, 0)
+  tmp$Sm <- ifelse(tmp$Outcome == "M", 1, 0)
+  tmp$SmX <- tmp$Sm * tmp$X
+  tmp$SyX <- tmp$Sy * tmp$X
+  tmp$SyM <- tmp$Sy * tmp$Md
+
+  return(tmp)
+}
+
+
