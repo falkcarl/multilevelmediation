eceaee51114bd2eb1798c52f384e26ef12d9dc8b
falkcarl <carl.falk@mcgill.ca>
2024-03-18 15:26:15 -0400

    update stack_bpg

--- R/utils.R
+++ R/utils.R
@@ -6,8 +6,9 @@
 #' @param Y (String) Name of column that contains the Y dependent variable in \code{data}.
 #' @param M (String) Name of column that contains the M mediating variable in \code{data}.
 #' @param moderator Optional Character that contains name of column that contains the moderator variable in \code{data}
-#' @param covars.m (Character vector) Optional covariates to include in the model for M.
-#' @param covars.y (Character vector) Optional covariates to include in the model for Y.
+#' @param covars.m (Character vector; deprecated) Optional covariates to include in the model for M.
+#' @param covars.y (Character vector; deprecated) Optional covariates to include in the model for Y.
+#' @param covars (Character vector) Optional covariates to include in either the model for M or for Y.
 #' @details This is a convenience function used primarily internally by the package to restructure data in the style of
 #'   Bauer, Preacher, and Gil (2006). The point is to allow both Y and M to be outcomes in a single column ("Z"),
 #'   so that both mediator and outcome models can be fit at the same time. This is necessary to estimate the covariance
@@ -30,7 +31,7 @@
 #'  \item{\code{Sm} Indicator variable for M as outcome. 0 if M is not outcome for this row. 1 if M is outcome for this row. In model output, this is the intercept for M.}
 #'  \item{\code{SmX} Value of X when M is the outcome (literally X times Sm); will be 0 when Y is outcome. In model output, this is the "a" path.}
 #'  \item{\code{SyX} Value of X when Y is the outcome (literally X times Sy); will be 0 when M is outcome. In model output, this is the "cprime" path (direct effect).}
-#'  \item{\code{SyM} Calue of M when Y is the outcome (literally M times Sy); will be 0 when M is the outcome. In model output, this is the "a" path.}
+#'  \item{\code{SyM} Value of M when Y is the outcome (literally M times Sy); will be 0 when M is the outcome. In model output, this is the "a" path.}
 #'  \item{\code{W} Value of any moderating variable. This may show up in output later on as "SmX:W" (interaction with "a" path) or "SyM:W" (interaction with "b" path) or "SyX:W"
 #'     (interaction with "cprime" path) depending on which path it moderates.}
 #'  \item{If \code{covars.m} or \code{covars.y} are not null, any additional covariates will also be added to the data frame and their
@@ -50,7 +51,7 @@
 #' data(BPG06dat)
 #'
 #' dat <- stack_bpg(BPG06dat,
-#'   "id", "x", "m", "y"
+#'   "id", "x", "y", "m"
 #' )
 #'
 #' head(dat)
@@ -58,7 +59,7 @@
 #' # restructure simulated data w/ moderator
 #' data(simdat)
 #' dat2 <- stack_bpg(simdat,
-#'   "L2id", "X", "M", "Y",
+#'   "L2id", "X", "Y", "M",
 #'   moderator = "mod"
 #' )
 #'
@@ -66,31 +67,25 @@
 #'
 #' }
 #' @importFrom tidyr pivot_longer
+#' @importFrom dplyr select mutate rename
+#' @importFrom tidyselect all_of
+#' @importFrom rlang .data
 #' @export stack_bpg
-#' @usage stack_bpg(data, L2ID, X, Y, M,
-#'   moderator = NULL,
-#'   covars.m = NULL,
-#'   covars.y = NULL)
 stack_bpg <-function(data, L2ID, X, Y, M,
                      moderator = NULL,
                      covars.m = NULL,
-                     covars.y = NULL){
+                     covars.y = NULL,
+                     covars = NULL){
 
   # Stop if X, Y, or M variables in data are not numeric
-  # factors not currently able to be used to set up BPG syntax for lme model (is there a possible workaround?)
+  # factors not currently able to be used to set up BPG syntax for lme model
   if (!is.numeric(data[[X]])) {stop("X is of type ", class(data[[X]]), ". Currently, only numeric X is supported.")}
   if (!is.numeric(data[[Y]])) {stop("Y is of type ", class(data[[Y]]), ". Y must be numeric to fit model.")}
   if (!is.numeric(data[[M]])) {stop("M is of type ", class(data[[M]]), ". M must be numeric to fit model.")}
 
-
-  # Use data frame with only relevant variables
-  tmp <- data.frame(
-    X = data[[X]],
-    Y = data[[Y]],
-    M = data[[M]],
-    L2id = data[[L2ID]], # Save copy of the grouping (Level 2) variable
-    Md = data[[M]] # save copy of mediator
-  )
+  # Save cols that we'll need
+  # Selection now at end
+  cols <- c(L2ID, X, Y, M)
 
   # Check and save moderator if necessary
   if (!is.null(moderator)) {
@@ -98,42 +93,70 @@ stack_bpg <-function(data, L2ID, X, Y, M,
     if (!(moderator %in% colnames(data))) {stop("Moderator not found in dataset. Please check specified name.")}
     if (!is.numeric(data[[moderator]])) {stop("Moderator is of type ", class(data[[moderator]]), ". Currently, only numeric moderators are supported.")}
 
-    # Save copy of the moderator
-    tmp$W <- data[[moderator]]
-  }# else if (any(mod.a, mod.b, mod.cprime)) {
-    # Give error if paths indicated as moderated, but no moderator name given
-  #  stop("No moderator was specified for the moderated path(s).")
-  #}
+    cols <- c(cols, moderator)
+
+  }
+
+  # check and save covariate
+  if (!is.null(covars)) {
+    # Check if covariate names are the same as existing vars
+    if (any(covars %in% c(X, Y, M, moderator))) {
+      stop("Covariate(s) have the same name as X, Y, M, or moderator variables.")
+    }
+    cols <- c(cols, covars)
+  }
+
+  # below deprecated, but we want to give time for those who use it to switch to newer syntax
 
   # Add covariates for M and Y outcome paths
   # (covars.m and covars.y should be a vector of column names in the dataset)
   if (!is.null(covars.m)) {
+    warning("covars.m with stack_bpg is deprecated and will be replaced with use of just covars in future package releases")
     # Check if covariate names are the same as existing vars
     if (any(covars.m %in% c(X, Y, M, moderator))) {
       stop("Covariate(s) have the same name as X, Y, M, or moderator variables.")
     }
     # Save copy of covariate data
-    tmp[covars.m] = data[covars.m] #will add same variable twice if same name in list (eg c("cov1", "cov2", "cov2")) (although renames by adding a ".1" to it, so may not be a problem when lme is run?)
+    #tmp[covars.m] = data[covars.m] #will add same variable twice if same name in list (eg c("cov1", "cov2", "cov2")) (although renames by adding a ".1" to it, so may not be a problem when lme is run?)
+
+    cols <- c(cols, covars.m)
   }
   if (!is.null(covars.y)) {
+    warning("covars.y with stack_bpg is deprecated and will be replaced with use of just covars in future package releases")
     # Check if covariate names are the same as existing vars
     if (any(covars.y %in% c(X, Y, M, moderator))) {
       stop("Covariate(s) have the same name as X, Y, M, or moderator variables.")
     }
     # Save copy of covariate data (still works if same covar is named for both m and y outcomes)
-    tmp[covars.y] = data[covars.y]
+    #tmp[covars.y] = data[covars.y]
+    cols <- c(cols, covars.y)
+  }
+
+  # end deprecated
+
+  # just variables we need
+  tmp <- dplyr::select(data, all_of(cols))
+  tmp["Md"] = tmp[M] # make copy of  mediator
+
+  # moderator: use naming convention
+  if (!is.null(moderator)) {
+    tmp <- rename(tmp, W = moderator)
   }
 
   # restructure data such that both m and y are in the Z column
   tmp <- pivot_longer(tmp, cols = c(Y, M), names_to = "Outcome",
                       values_to = "Z")
 
-  # create variables similar to Bauer et al syntax
-  tmp$Sy <- ifelse(tmp$Outcome == "Y", 1, 0)
-  tmp$Sm <- ifelse(tmp$Outcome == "M", 1, 0)
-  tmp$SmX <- tmp$Sm * tmp$X
-  tmp$SyX <- tmp$Sy * tmp$X
-  tmp$SyM <- tmp$Sy * tmp$Md
+  # create variables similar to Bauer et al syntax; enforce naming conventions
+  tmp <- rename(tmp, X = X,
+                L2id = L2ID)
+  tmp <- mutate(tmp,
+                Sy = ifelse(.data$Outcome == Y, 1, 0),
+                Sm = ifelse(.data$Outcome == M, 1, 0))
+  tmp <- mutate(tmp,
+                SmX = .data$Sm * .data$X,
+                SyX = .data$Sy * .data$X,
+                SyM = .data$Sy * .data$Md)
 
   return(tmp)
 }
