604c6fd437430847d8ebc8bdf9a473fa282f8fde
falkcarl <carl.falk@mcgill.ca>
2025-01-10 06:50:32 -0500

    other fixes for CRAN

--- R/BPG06dat.R
+++ R/BPG06dat.R
@@ -10,7 +10,7 @@
 #'   \item{m}{Mediator}
 #'   \item{y}{Outcome}
 #' }
-#' @source \url{https://doi.org/10.1037/1082-989X.11.2.142}
+#' @source \doi{doi:10.1037/1082-989X.11.2.142}
 #' @references
 #' Bauer, D. J., Preacher, K. J., & Gil, K. M. (2006). Conceptualizing and testing random indirect effects and moderated mediation in multilevel models: New procedures and recommendations. Psychological Methods, 11(2), 142–163. \doi{https://doi.org/10.1037/1082-989X.11.2.142}
 "BPG06dat"
