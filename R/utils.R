d009754394c2dbb24222e53c0caa8a9e970a8393
toddvogel1628 <33038588+toddvogel1628@users.noreply.github.com>
2025-12-07 15:06:54 +0000

    Formatted with Air.

--- R/utils.R
+++ R/utils.R
@@ -66,23 +66,32 @@
 #'   moderator = NULL,
 #'   covars.m = NULL,
 #'   covars.y = NULL)
-stack_bpg <- function(data, L2ID, X, Y, M,
-                      moderator = NULL,
-                      covars.m = NULL,
-                      covars.y = NULL){
-
+stack_bpg <- function(data, L2ID, X, Y, M, moderator = NULL, covars.m = NULL, covars.y = NULL) {
   # Check L2ID, X, Y, M exist in data
-  if (!(L2ID %in% colnames(data))) {stop("L2ID variable not found in dataset. Please check specified name.")}
-  if (!(X %in% colnames(data))) {stop("X variable not found in dataset. Please check specified name.")}
-  if (!(Y %in% colnames(data))) {stop("Y variable not found in dataset. Please check specified name.")}
-  if (!(M %in% colnames(data))) {stop("M variable not found in dataset. Please check specified name.")}
+  if (!(L2ID %in% colnames(data))) {
+    stop("L2ID variable not found in dataset. Please check specified name.")
+  }
+  if (!(X %in% colnames(data))) {
+    stop("X variable not found in dataset. Please check specified name.")
+  }
+  if (!(Y %in% colnames(data))) {
+    stop("Y variable not found in dataset. Please check specified name.")
+  }
+  if (!(M %in% colnames(data))) {
+    stop("M variable not found in dataset. Please check specified name.")
+  }
 
   # Stop if X, Y, or M variables in data are not numeric
   # factors not currently able to be used to set up BPG syntax for lme model (is there a possible workaround?)
-  if (!is.numeric(data[[X]])) {stop("X is of type ", class(data[[X]]), ". Currently, only numeric X is supported.")}
-  if (!is.numeric(data[[Y]])) {stop("Y is of type ", class(data[[Y]]), ". Y must be numeric to fit model.")}
-  if (!is.numeric(data[[M]])) {stop("M is of type ", class(data[[M]]), ". M must be numeric to fit model.")}
-
+  if (!is.numeric(data[[X]])) {
+    stop("X is of type ", class(data[[X]]), ". Currently, only numeric X is supported.")
+  }
+  if (!is.numeric(data[[Y]])) {
+    stop("Y is of type ", class(data[[Y]]), ". Y must be numeric to fit model.")
+  }
+  if (!is.numeric(data[[M]])) {
+    stop("M is of type ", class(data[[M]]), ". M must be numeric to fit model.")
+  }
 
   # Use data frame with only relevant variables
   tmp <- data.frame(
@@ -96,46 +105,62 @@ stack_bpg <- function(data, L2ID, X, Y, M,
   # Check and save moderator if necessary
   if (!is.null(moderator)) {
     # Check validity of moderator
-    if (length(moderator) > 1) {stop("Only 1 moderator variable is allowed.")}
-    if (!(moderator %in% colnames(data))) {stop("Moderator not found in dataset. Please check specified name.")}
-    if (!is.numeric(data[[moderator]])) {stop("Moderator is of type ", class(data[[moderator]]), ". Currently, only numeric moderators are supported.")}
-    if (moderator %in% c(X, Y, M, covars.m, covars.y, L2ID)) {stop("Moderator has same name as X, Y, M, covars.m, or covars.y, or L2ID variables.")}
+    if (length(moderator) > 1L) {
+      stop("Only 1 moderator variable is allowed.")
+    }
+    if (!(moderator %in% colnames(data))) {
+      stop("Moderator not found in dataset. Please check specified name.")
+    }
+    if (!is.numeric(data[[moderator]])) {
+      stop("Moderator is of type ", class(data[[moderator]]), ". Currently, only numeric moderators are supported.")
+    }
+    if (moderator %in% c(X, Y, M, covars.m, covars.y, L2ID)) {
+      stop("Moderator has same name as X, Y, M, covars.m, or covars.y, or L2ID variables.")
+    }
 
     # Save copy of the moderator
     tmp$W <- data[[moderator]]
-  }# else if (any(mod.a, mod.b, mod.cprime)) {
-    # Give error if paths indicated as moderated, but no moderator name given
-  #  stop("No moderator was specified for the moderated path(s).")
-  #}
+  }
 
   # Add covariates for M and Y outcome paths
   # (covars.m and covars.y should be a vector of column names in the dataset)
   if (!is.null(covars.m)) {
     # Check validity of m covariates
-    if (!all(covars.m %in% colnames(data))) {stop("One or more covariates not found in dataset. Please check specified name(s).")}
-    if (any(!sapply(data[covars.m], is.numeric))) {stop("One or more covariates is not numeric. Currently, only numeric covariates are supported.")}
-    if (any(covars.m %in% c(X, Y, M, moderator, L2ID))) {stop("Covariate(s) have the same name as X, Y, M, moderator, or L2ID variables.")}
+    if (!all(covars.m %in% colnames(data))) {
+      stop("One or more covariates not found in dataset. Please check specified name(s).")
+    }
+    if (!all(sapply(data[covars.m], is.numeric))) {
+      stop("One or more covariates is not numeric. Currently, only numeric covariates are supported.")
+    }
+    if (any(covars.m %in% c(X, Y, M, moderator, L2ID))) {
+      stop("Covariate(s) have the same name as X, Y, M, moderator, or L2ID variables.")
+    }
 
     # Save copy of covariate data
     tmp[covars.m] <- data[covars.m] #will add same variable twice if same name in list (eg c("cov1", "cov2", "cov2")) (although renames by adding a ".1" to it, so may not be a problem when lme is run?)
   }
   if (!is.null(covars.y)) {
     # Check validity of y covariates
-    if (!all(covars.y %in% colnames(data))) {stop("One or more covariates not found in dataset. Please check specified name(s).")}
-    if (any(!sapply(data[covars.y], is.numeric))) {stop("One or more covariates is not numeric. Currently, only numeric covariates are supported.")}
-    if (any(covars.y %in% c(X, Y, M, moderator, L2ID))) {stop("Covariate(s) have the same name as X, Y, M, moderator, or L2ID variables.")}
+    if (!all(covars.y %in% colnames(data))) {
+      stop("One or more covariates not found in dataset. Please check specified name(s).")
+    }
+    if (!all(sapply(data[covars.y], is.numeric))) {
+      stop("One or more covariates is not numeric. Currently, only numeric covariates are supported.")
+    }
+    if (any(covars.y %in% c(X, Y, M, moderator, L2ID))) {
+      stop("Covariate(s) have the same name as X, Y, M, moderator, or L2ID variables.")
+    }
 
     # Save copy of covariate data (still works if same covar is named for both m and y outcomes)
     tmp[covars.y] <- data[covars.y]
   }
 
   # restructure data such that both m and y are in the Z column
-  tmp <- tidyr::pivot_longer(tmp, cols = c(Y, M), names_to = "Outcome",
-                      values_to = "Z")
+  tmp <- tidyr::pivot_longer(tmp, cols = c(Y, M), names_to = "Outcome", values_to = "Z")
 
   # create variables similar to Bauer et al. syntax
-  tmp$Sy <- ifelse(tmp$Outcome == "Y", 1, 0)
-  tmp$Sm <- ifelse(tmp$Outcome == "M", 1, 0)
+  tmp$Sy <- as.numeric(tmp$Outcome == "Y")
+  tmp$Sm <- as.numeric(tmp$Outcome == "M")
   tmp$SmX <- tmp$Sm * tmp$X
   tmp$SyX <- tmp$Sy * tmp$X
   tmp$SyM <- tmp$Sy * tmp$Md
